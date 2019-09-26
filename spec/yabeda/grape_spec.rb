93aa5abff0a4c4c2efbb595e11b63b9ec7aa4e4a
Vadym Filipov <vfilipov@efigence.com>
2019-09-26 10:28:41 +0200

    clean test

--- spec/yabeda/grape_spec.rb
+++ spec/yabeda/grape_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Yabeda::Grape do
   it "has a version number" do
     expect(Yabeda::Grape::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
